33ad70b141926717b40eaaf9ef63861d375b7b7b
fidel.alex <alekseifidel@gmail.com>
2023-08-08 20:22:38 +0500

    dz_002

---
+++ exercise_002/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите ваше число");
+
+string number = Console.ReadLine();
+
+int checkUserNumber;
+                         
+bool userNumber = int.TryParse(number, out checkUserNumber);          
+
+if (userNumber == false)
+{
+    Console.WriteLine("Болбес введи число !!!");
+}
+while (userNumber)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (i == 2)
+            {
+                Console.WriteLine(number[i]);
+            }
+            else if (i >= number.Length - 1)
+            {
+                Console.WriteLine("третьей цифры нет!!!");
+            }
+    }
+    userNumber = false;
+}
