f692a9377ec3a479d10f23edc4a1ab29fd77b48c
fidel.alex <alekseifidel@gmail.com>
2023-08-08 20:42:36 +0500

    dz_003

---
+++ exercise_003/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string numberOfWeek = Console.ReadLine();
+
+switch(numberOfWeek)
+{
+    case "1":
+        Console.WriteLine("Нет");
+    break;
+    case "2":
+        Console.WriteLine("Нет");
+    break;
+    case "3":
+        Console.WriteLine("Нет");
+    break;
+    case "4":
+        Console.WriteLine("Нет");
+    break;
+    case "5":
+        Console.WriteLine("Нет");
+    break;
+    case "6":
+        Console.WriteLine("Да, это суббота!!!");
+    break;
+    case "7":
+        Console.WriteLine("Да, это воскресенье!!!");
+    break;
+    default:
+        Console.WriteLine("Такого дня недели нет");
+    break;
+}
+
